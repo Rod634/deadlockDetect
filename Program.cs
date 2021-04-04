2d97a293b5823a8f0b3ecaece5599d89ed991b59
Rodrigo Silva <rodrigocsilva63@gmail.com>
2021-04-03 21:12:18 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace deadlockDetect
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
